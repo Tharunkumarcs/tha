f3355ccc5f364415526b3c2c398038a41e7c9bb0
Tharunkumarcs <47142208+Tharunkumarcs@users.noreply.github.com>
2019-02-11 16:27:02 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,6 +5,10 @@ void main()
 char a;
 printf("\n enter the character");
 scanf("%c",&a);
+if(isalpha(c)==0)
+{
+ printf("\n it is invalid");
+}
 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
 {
 printf("\n it is vowel");
